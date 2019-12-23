c648e32f71f1e3108629d20312c9537113696982
Lyudmil Antonov <lantonov.here@gmail.com>
2019-12-23 09:16:36 +0200

    Tuned reductions_table Bench 5547205

--- src/search.cpp
+++ src/search.cpp
@@ -75,7 +75,7 @@ namespace {
 
   Depth reduction(bool i, Depth d, int mn) {
     int r = Reductions[d] * Reductions[mn];
-    return (r + 451) / 1024 + (!i && r > 1117);
+    return (r + 594) / 1024 + (!i && r > 1194);
   }
 
   constexpr int futility_move_count(bool improving, Depth depth) {
