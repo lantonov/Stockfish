383887764140b0e7ea4f1f47e33b5de05b22ec4f
lantonov <lantonov.here@gmail.com>
2020-01-04 21:55:05 +0200

    Tuned RookOnFile; (40, 1), (93, 47) Bench 5041280

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -108,7 +108,7 @@ namespace {
 
   // RookOnFile[semiopen/open] contains bonuses for each rook when there is
   // no (friendly) pawn on the rook file.
-  constexpr Score RookOnFile[] = { S(21, 4), S(47, 25) };
+  constexpr Score RookOnFile[] = { S(40, 1), S(93, 47) };
 
   // ThreatByMinor/ByRook[attacked PieceType] contains bonuses according to
   // which piece type attacks which one. Attacks on lesser pieces which are
