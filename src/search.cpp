b615ccb8ced8e0b3bcbc2de17518b459fcb3a916
lantonov <lantonov.here@gmail.com>
2020-01-05 18:05:24 +0200

    Tuned reductions lookup table; 1022, 1932 Bench 4356629

--- src/search.cpp
+++ src/search.cpp
@@ -75,7 +75,7 @@ namespace {
 
   Depth reduction(bool i, Depth d, int mn) {
     int r = Reductions[d] * Reductions[mn];
-    return (r + 144) / 1024 + (!i && r > 782);
+    return (r + 1022) / 1024 + (!i && r > 1932);
   }
 
   constexpr int futility_move_count(bool improving, Depth depth) {
