bb1ab7acba4b7a1400f68c3be04b1a53cb8a328b
lantonov <lantonov.here@gmail.com>
2020-01-16 23:10:30 +0200

    Tuned Kingattackweights; 7, 1, 81, 3 Bench 5403066

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -78,7 +78,7 @@ namespace {
   constexpr Value SpaceThreshold = Value(12222);
 
   // KingAttackWeights[PieceType] contains king attack weights by piece type
-  constexpr int KingAttackWeights[PIECE_TYPE_NB] = { 0, 0, 162, 2, 0, 2 };
+  constexpr int KingAttackWeights[PIECE_TYPE_NB] = { 0, 0, 7, 1, 81, 3 };
 
   // Penalties for enemy's safe checks
   constexpr int QueenSafeCheck  = 780;
